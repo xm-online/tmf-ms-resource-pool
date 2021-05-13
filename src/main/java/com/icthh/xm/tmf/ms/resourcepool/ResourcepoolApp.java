447e2ca83e92c8bd3197161410aa9c3e86178427
MaximBogun <maximboguns@gmail.com>
2021-05-13 10:33:48 +0300

    Add LiquibaseProperties.class

--- src/main/java/com/icthh/xm/tmf/ms/resourcepool/ResourcepoolApp.java
+++ src/main/java/com/icthh/xm/tmf/ms/resourcepool/ResourcepoolApp.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.core.env.Environment;
 
@@ -21,7 +22,7 @@ import java.util.Arrays;
 import java.util.Collection;
 
 @SpringBootApplication(scanBasePackages = { "com.icthh.xm", "com.icthh.xm.tmf.ms.resourcepool" })
-@EnableConfigurationProperties({ApplicationProperties.class})
+@EnableConfigurationProperties({{LiquibaseProperties.class, ApplicationProperties.class})
 @EnableAutoConfiguration
 public class ResourcepoolApp {
 
