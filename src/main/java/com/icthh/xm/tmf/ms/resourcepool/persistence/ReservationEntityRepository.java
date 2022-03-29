9a3c7f21a3fe3239bf0f3bd96623bc157224bc4a
Yurii Matora <ymatora@itsmartflex.com>
2022-03-29 13:48:45 +0300

    Add reservation filtering by commerces

--- src/main/java/com/icthh/xm/tmf/ms/resourcepool/persistence/ReservationEntityRepository.java
+++ src/main/java/com/icthh/xm/tmf/ms/resourcepool/persistence/ReservationEntityRepository.java
@@ -1,14 +1,15 @@
 package com.icthh.xm.tmf.ms.resourcepool.persistence;
 
-import java.time.OffsetDateTime;
-import java.util.List;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import java.time.OffsetDateTime;
+import java.util.List;
 
 @Repository
 @Transactional(readOnly = true)
@@ -20,14 +21,22 @@ public class ReservationEntityRepository {
     /**
      * Get reservation with limit and offset
      *
-     * @param status
-     * @return
+     * @param status            Reservation status
+     * @param commerces         Possible reservation commerces
+     * @param currentDate       Current date
+     * @param isTimeoutNotified Is timeout notified
+     * @param limit             Limit of max results
+     * @return Reservations
      */
-    public List<Reservation> findBy(Status status, OffsetDateTime currentDate, Boolean isTimeoutNotified, Integer limit) {
+    public List<Reservation> findBy(Status status, List<Commerce> commerces, OffsetDateTime currentDate,
+                                    Boolean isTimeoutNotified, Integer limit) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Reservation> cq = cb.createQuery(Reservation.class);
         Root<Reservation> root = cq.from(Reservation.class);
-        cq.where(cb.equal(root.get("status"), status), cb.lessThanOrEqualTo(root.get("createDate"), currentDate), cb.equal(root.get("isTimeoutNotified"), isTimeoutNotified));
+        cq.where(cb.equal(root.get(Reservation_.status), status),
+            root.get(Reservation_.commerce).in(commerces),
+            cb.lessThanOrEqualTo(root.get(Reservation_.createDate), currentDate),
+            cb.equal(root.get(Reservation_.isTimeoutNotified), isTimeoutNotified));
         cq.select(root);
         return entityManager.createQuery(cq).setMaxResults(limit).getResultList();
     }
@@ -35,14 +44,18 @@ public class ReservationEntityRepository {
     /**
      * Get expired reservations
      *
-     * @param status
-     * @return
+     * @param status    Reservation status
+     * @param commerces Possible reservation commerces
+     * @param limit     Limit of max results
+     * @return Reservations
      */
-    public List<Reservation> findExpiredReservationsBy(Status status, Integer limit) {
+    public List<Reservation> findExpiredReservationsBy(Status status, List<Commerce> commerces, Integer limit) {
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<Reservation> cq = cb.createQuery(Reservation.class);
         Root<Reservation> root = cq.from(Reservation.class);
-        cq.where(cb.equal(root.get("status"), status), cb.lessThanOrEqualTo(root.get("validFor"), OffsetDateTime.now()));
+        cq.where(cb.equal(root.get(Reservation_.status), status),
+            root.get(Reservation_.commerce).in(commerces),
+            cb.lessThanOrEqualTo(root.get(Reservation_.validFor), OffsetDateTime.now()));
         cq.select(root);
         return entityManager.createQuery(cq).setMaxResults(limit).getResultList();
     }
